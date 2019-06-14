a73978d19172c50c67b6fbe19ffb3da918b5a070
Martynas Kasparavičius <martis.kasparavicius@gmail.com>
2019-06-14 13:27:12 +0300

    Form created, styled

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\CoordinatesType;
+use phpDocumentor\Reflection\Types\Array_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
@@ -10,10 +13,24 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="appMain")
      */
-    public function index()
+    public function index(Request $request)
     {
+        $visitedBreweries = [];
+        $collectedBeer = [];
+
+        $form = $this->createForm(CoordinatesType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+
+        }
+
         return $this->render('main/index.html.twig', [
-            'pageTitle' => 'Beer test'
+            'pageTitle' => 'Beer test',
+            'coordForm' => $form->createView(),
+            'visitedBreweries' => $visitedBreweries,
+            'collectedBeer' => $collectedBeer,
         ]);
     }
 }
